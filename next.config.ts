5a5dc30685c99cdadfa360e22a278e7de5d77b2d
ikhsankhaeruddin04 <ikhsankhaeruddin04@gmail.com>
2025-12-19 21:46:57 +0700

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",       // agar bisa export static HTML
+  // basePath: "/portfolio", // optional, sesuai nama repo GitHub
 };
 
 export default nextConfig;
