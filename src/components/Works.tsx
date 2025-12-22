b76cb4a14228edad5730d5068abf4ca8542f19d1
Ikhsan Khaeruddin <ikhsankhaeruddin04@gmail.com>
2025-12-22 22:09:35 +0700

    Update

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -3,18 +3,45 @@ import { motion } from "framer-motion";
 
 export default function Works() {
   const works = [
+    // {
+    //   img: "/work1.png",
+    //   title: "Qutopia – Educational HTML5 Game",
+    //   category: "Game Development",
+    //   link: "https://ikhssan.itch.io/qutopia",
+    // },
+    // {
+    //   img: "/work2.png",
+    //   title: "Portfolio Website",
+    //   category: "Web Development",
+    //   link: "https://portfolio-71d6.vercel.app/",
+    // },
     {
-      img: "/work1.png",
-      title: "Qutopia – Educational HTML5 Game",
-      category: "Game Development",
-      link: "https://ikhssan.itch.io/qutopia",
-    },
-    {
-      img: "/work2.png",
-      title: "Portfolio Website",
-      category: "Web Development",
-      link: "https://portfolio-71d6.vercel.app/",
-    },
+    img: "/work1.png",
+    title: "Qutopia – Educational HTML5 Game",
+    category: "Game Development",
+    description:
+      "Educational game designed to improve student learning through interactive stages and scoring mechanics.",
+    stack: ["Unity", "C#", "Game Design"],
+    link: "https://ikhssan.itch.io/qutopia",
+  },
+  {
+    img: "/work2.png",
+    title: "Portfolio Website",
+    category: "Web Development",
+    description:
+      "Personal portfolio website to showcase projects, skills, and experiences with responsive design.",
+    stack: ["Next.js", "React", "Tailwind CSS"],
+    link: "https://portfolio-71d6.vercel.app/",
+  },
+  {
+    img: "/work3.png",
+    title: "Qutopia – Game-Based Learning & Student Monitoring System",
+    category: "Web Development",
+    description:
+      "Web-based monitoring system integrated with the Qutopia game, enabling teachers to track student mastery, stages, and learning progress through an admin dashboard.",
+    stack: ["PHP", "MySQL", "JavaScript", "REST API"],
+    link: "https://mclquiz.my.id/quiz/dashboard.php",
+  },
     
   ];
 
